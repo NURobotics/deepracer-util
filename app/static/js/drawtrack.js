ebd6065bdfee247aa62df0321427b8efdce00b9a
maxpaik16 <maxpaik@gmail.com>
2019-11-07 21:03:47 -0600

    draw track

--- app/static/js/drawtrack.js
+++ app/static/js/drawtrack.js
@@ -17,20 +17,6 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 
 
-//Takes in many points and draws a curve through them
-function drawCurve(points, canvas, ctx) {
-  ctx.moveTo(points[0][0], points[0][1]);
-  for (i = 0; i < points.length - 2; ++i) {
-    ctx.bezierCurveTo(
-      points[i][0], points[i][1],
-      points[i + 1][0], points[i + 1][1],
-      points[i + 2][0], points[i + 2][1]
-    )
-    ctx.stroke()
-  }
-}
-
-
 //Takes in three (ordered) points in and
 //returns the estimated outer and inner points on the track
 //relative to the middle point
@@ -69,6 +55,13 @@ function norm(vector) {
   return [vector[0] / x, vector[1] / x];
 }
 
+function distance(point1, point2) {
+  return Math.sqrt((point1[0] - point2[0]) ** 2 +
+                   (point1[1] - point2[0]) ** 2)
+}
+
+
+
 class track {
   constructor(points, params) {
     this._points = points;
@@ -80,15 +73,13 @@ class track {
   set points(pointList) {
     this._points = pointList;
   }
-  drawTrackPoints() {
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
+  getEdgePoints() {
+    var points1 = [];
+    var points2 = [];
     for (i = 1; i < (this._points.length - 1); ++i) {
       var temp = getEdgeEstimate(this._points.slice(i - 1, i + 2));
-      context.fillStyle = "blue";
-      context.fillRect(temp[0][0], temp[0][1], 3, 3);
-      context.fillStyle = "red";
-      context.fillRect(temp[1][0], temp[1][1], 3, 3);
+      points1.push([temp[0][0], temp[0][1]]);
+      points2.push([temp[1][0], temp[1][1]]);
     }
     var outline1 = getEdgeEstimate(
         [this._points[this._points.length - 1],
@@ -98,20 +89,103 @@ class track {
         [this._points[this._points.length - 2],
          this._points[this._points.length - 1], this._points[0]]
       );
-    context.fillStyle = "blue";
-    context.fillRect(outline1[0][0], outline1[0][1], 3, 3);
-    context.fillRect(outline2[0][0], outline2[0][1], 3, 3);
-    context.fillStyle = "red";
-    context.fillRect(outline1[1][0], outline1[1][1], 3, 3);
-    context.fillRect(outline2[1][0], outline2[1][1], 3, 3);
+    points1.push([outline1[0][0], outline1[0][1]]);
+    points1.push([outline2[0][0], outline2[0][1]]);
+    points2.push([outline1[1][0], outline1[1][1]]);
+    points2.push([outline2[1][0], outline2[1][1]]);
+    return [points1, points2];
+  }
+  drawTrackPoints() {
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    var points1 = this.getEdgePoints()[0];
+    var points2 = this.getEdgePoints()[1];
+    for (i = 0; i < points1.length; ++i) {
+      context.fillStyle = "blue";
+      context.fillRect(points1[i][0], points1[i][1], 3, 3);
+      context.fillStyle = "red";
+      context.fillRect(points2[i][0], points2[i][1], 3, 3);
+    }
+  }
+  getCenterPoint() {
+    var xSum = 0;
+    var ySum = 0;
+    for (i = 0; i < this._points.length; ++i) {
+      xSum += this._points[i][0];
+      ySum += this._points[i][1];
+    }
+    var x = xSum / this._points.length;
+    var y = ySum / this._points.length;
+    return [x, y];
+  }
+  // return True if points1 is the outer ring, false if otherwise
+  InnerVsOuter() {
+    var points1 = this.getEdgePoints()[0];
+    var points2 = this.getEdgePoints()[1];
+    var center = this.getCenterPoint();
+    var distance1 = 0;
+    var distance2 = 0;
+    for (i = 0; i < points1.length; ++i) {
+      distance1 += distance(points1[i], center);
+      distance2 += distance(points2[i], center);
+    }
+    if (distance1 > distance2) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
   drawTrackCurve() {
     //stuff here
-    return 0;
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+    if (this.InnerVsOuter()) {
+      var inner = this.getEdgePoints()[1];
+      var outer = this.getEdgePoints()[0];
+    }
+    else {
+      var inner = this.getEdgePoints()[0];
+      var outer = this.getEdgePoints()[1];
+    }
+    context.beginPath();
+    context.lineWidth= 3;
+    context.lineCap = 'round';
+    context.fillStyle = 'black';
+    for (i = 0; i < outer.length; ++i) {
+      context.lineTo(outer[i][0], outer[i][1]);
+    }
+    context.closePath();
+    context.fill();
+    context.beginPath();
+    context.fillStyle = 'green';
+    for (i = 0; i < inner.length; ++i) {
+      context.lineTo(inner[i][0], inner[i][1]);
+    }
+    context.closePath();
+    context.fill();
+  }
+}
+
+
+class car {
+  constructor(x, y, orientation) {
+    this._x = x;
+    this._y = y;
+    this._orientation = orientation;
+  }
+  get position() {
+    return [this._x, this._y];
+  }
+  set position(coord) {
+    this._x = coord[0];
+    this._y = coord[1];
+  }
+  drawCar() {
+    //stuff here
   }
 }
 
 
 var testTrack = new track(point_list, 0);
-testTrack.drawTrackPoints();
-drawCurve(testTrack.points, canvas, context);
+testTrack.drawTrackCurve();
